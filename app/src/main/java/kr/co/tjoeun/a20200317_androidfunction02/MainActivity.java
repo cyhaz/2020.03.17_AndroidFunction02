78a0c52f0ef0e89aa46876fa5ea18afc42d680fa
cyhaz <cyhaz@naver.com>
2020-03-17 17:45:02 +0900

    전화 직접 거는 코드  - 권한 문제로 지금은 실행 불가

--- app/src/main/java/kr/co/tjoeun/a20200317_androidfunction02/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200317_androidfunction02/MainActivity.java
@@ -38,5 +38,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        binding.callBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                현재까지 배운것만으로는 실행 불가 : 권한 획득 x
+
+                Uri uri = Uri.parse("tel:01022612183");
+                Intent intent = new Intent(Intent.ACTION_CALL, uri);
+                startActivity(intent);
+            }
+        });
+
     }
 }
